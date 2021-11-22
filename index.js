a3a9667cdf5d1be1d673c2349972c30ae4ba2ac4
Tim B <t.beuke15433@hs-ansbach.de>
2021-11-22 19:54:02 +0100

    fullfills task f

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-const {diamond} = require('./diamond.js')
+const {buildCharArray, buildQuadrant, buildYMirroredImage, buildXMirroredImage, diamond} = require('./diamond.js')
 
 const diamondLetter = process.argv[2]
 const replacmentLetter = process.argv[3]
+const diamondArray = buildXMirroredImage(buildYMirroredImage(buildQuadrant(buildCharArray(diamondLetter))))
 
 if(replacmentLetter != null){
-    .forEach(element => (element === '-') ? element = replacmentLetter : null)
+    diamondArray.forEach((arrayElement) => {arrayElement.forEach((element, index) => {(element === '-') ? arrayElement[index] = replacmentLetter : null})})
 }
-diamond()
+
+diamond(diamondArray)
+
